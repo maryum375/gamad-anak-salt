b5d808a5c8c767c65217610190d8914aa61a3aaa
Ran Mariuma <ranm@salt.security>
2021-02-24 00:18:30 +0200

    Changed api url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const checkLordName = (lordName, callback) => {
-  axios.post('http://localhost:8080/check', { lordName })
+  axios.post('https://gamad-anak-salt.herokuapp.com/check', { lordName })
     .then(function (response) {
       console.log(response);
       callback(response)
