a8a9474499d6dd2bd02db4496bf46fab6eb81a25
Ran Mariuma <ranm@salt.security>
2021-02-24 11:18:44 +0200

    api.js: Change the server url

--- src/api.js
+++ src/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const checkLordName = (lordName, callback) => {
-  // axios.post('https://gamad-anak-salt.herokuapp.com/check', { lordName })
-  axios.post('http://localhost:8080/check', { lordName })
+  axios.post('https://gamad-anak-salt.herokuapp.com/check', { lordName })
     .then(function (response) {
       console.log(response);
       callback(response)
@@ -13,8 +12,7 @@ const checkLordName = (lordName, callback) => {
 }
 
 const decryptMessage = (encrypted, key, callback) => {
-  // axios.post('https://gamad-anak-salt.herokuapp.com/decrypt', { lordName })
-  axios.post('http://localhost:8080/decrypt', { encrypted, key })
+  axios.post('https://gamad-anak-salt.herokuapp.com/decrypt', { encrypted, key })
     .then(function (response) {
       console.log(response);
       callback(response)
